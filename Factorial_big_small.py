b58d5dfe2affe21a46d5125779434fab340821a8
Prashasti Khunteta <31439694+Prashastikh@users.noreply.github.com>
2020-05-12 20:53:03 +0530

    Update Factorial_big_small.py

--- Factorial_big_small.py
+++ Factorial_big_small.py
@@ -1,6 +1,17 @@
-n = int(input())
-ans = 1
-for i in range(1,n+1):
-    ans = ans*i
+# n = int(input())
+# ans = 1
+# for i in range(1,n+1):
+#     ans = ans*i
 
-print(ans)
+# print(ans)
+# Using function
+def fact(n):
+    ans = 1
+    for i in range(1,n+1):
+        ans = ans*i
+    return ans
+
+n = int(input())
+print(fact(n))
+    
+    
