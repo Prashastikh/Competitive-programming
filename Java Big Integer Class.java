d8a89ae5dd2cfd4023af4def820ba99e0798c8cf
Prashasti Khunteta <31439694+Prashastikh@users.noreply.github.com>
2020-05-13 12:42:01 +0530

    Add files via upload

---
+++ Java Big Integer Class.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.math.BigInteger;
+public class Main {
+    public static void playwithnumber()
+    {
+        Scanner scn = new Scanner(System.in);
+        String a = scn.next();
+        BigInteger b1 = new BigInteger(a);
+        BigInteger b2 = new BigInteger("5");
+
+        //Arithmetic Operations
+        System.out.println(b1.add(b2));
+        System.out.println(b1.subtract(b2));
+        System.out.println(b1.multiply(b2));
+        System.out.println(b1.divide(b2));
+
+        // bit Count - Total Set Bits
+        System.out.println(b1.bitCount());
+        // Total number of bits
+        System.out.println(b1.bitLength());
+
+        //Converting Int to BigInteger
+        int c = scn.nextInt();
+        int d = scn.nextInt();
+        System.out.println(c+d);
+        //Conversions
+        BigInteger b3 = BigInteger.valueOf(c);
+        BigInteger b4 = BigInteger.valueOf(d);
+        //print GCD
+        System.out.println(b3.gcd(b4));
+
+        // base conversion. Interprets number in given way
+        BigInteger b5 = new BigInteger("1001",2)
+        System.out.println(b5);
+
+        //Power
+        //Parameter inside pow should be an integer . Answer would be biginteger only
+        System.out.println(b3.pow(20));
+
+    }
+    public static void main(String args[]) {
+        playwithnumber();
+    }
+}
