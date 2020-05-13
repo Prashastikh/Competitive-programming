757215c2394951db8b7512585651e2c7424a553a
Prashasti Khunteta <31439694+Prashastikh@users.noreply.github.com>
2020-05-13 13:03:33 +0530

    Update Java Big Integer Class.java

--- Java Big Integer Class.java
+++ Java Big Integer Class.java
@@ -38,7 +38,17 @@ public class Main {
         System.out.println(b3.pow(20));
 
     }
+    public static BigInteger fact(int n)
+    {
+        BigInteger b = new BigInteger("1");
+        for(int i=2;i<=n;i++)
+        {
+            b = b.multiply(BigInteger.valueOf(i));
+        }
+        return b;
+    }
     public static void main(String args[]) {
         playwithnumber();
+         System.out.println(fact(100));
     }
 }
